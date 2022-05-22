99348ed05fb1096bc6fc759e205881e3072d3479
koloblack <89445632+koloblack@users.noreply.github.com>
2022-05-23 00:34:37 +0800

    basic multiplayer logic implementation

--- contracts/GameMaster.sol
+++ contracts/GameMaster.sol
@@ -3,95 +3,123 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "./BallBoard.sol";
+import "./Queue.sol";
+
 
 contract GameMaster {
-
+    using Queue for Queue.Uint256Queue;
+    Queue.Uint256Queue runningGameQueue;
     BallBoard ballBoardContract;
+   
 
     struct Game {
         uint256 boardId;
-        address owner;
-        address winner;
         uint256 prizePool;
         uint256 numPlayers;
-        uint256 entryCost;
         bool isGameRunning;
+
+        address[] _playerList;
+        address[] winnerList;
     }
 
-    mapping(uint256 => Game) private games;
+    mapping(uint256 => Game) public games;
+    mapping(address => bool) public playerList;
 
+    address contractOwner;
     uint8 public numCups = 8;
-    uint256 private entryCommission;
+    uint256 public entryCommission;
+    uint256 public entryCost;
+    uint256 public numOfRunningGames;
+    uint256 public contractOwnerCommission;
 
-    modifier gameOwnerOnly(uint256 gameId) {
-        require(msg.sender == games[gameId].owner, "Message sender not owner");
-        _;
-    }
-
-    constructor(uint256 _entryCommission, BallBoard ballBoardContractAddress) {
-        // fixed commission for POC simplicity
-        entryCommission = _entryCommission;
+    constructor(BallBoard ballBoardContractAddress) {
+        // fixed entryCost for simplicity
+        // 10000000000000 Wei / 0.00001 Ether
+        // Queue implementation always added a 0 so dequeued
+        entryCost = 10000000000000;
         ballBoardContract = ballBoardContractAddress;
+        runningGameQueue.dequeue();
     }
 
     // owner only
-    function startGame(uint256 newEntryCost) public payable returns (uint256 newGameId) {
-        // set running flag
-        // inject starting pool
-        // set new entry cost
-        require(msg.value > 0, "Initial pot missing");
-
+    // no longer need to be owner only
+    function startGame() private returns (uint256 newGameId) {
         //TODO: new logic for gameId
         newGameId = ballBoardContract.createBoard(numCups);
         
         Game storage newGame = games[newGameId];
         newGame.boardId = newGameId;
-        newGame.owner = msg.sender;
-        newGame.winner = address(0);
-        newGame.prizePool += msg.value;
-        newGame.entryCost = newEntryCost;
         newGame.isGameRunning = true;
-
+        numOfRunningGames++;
+        runningGameQueue.enqueue(newGameId);
     }
 
-    function endGame(uint256 gameId) public {
-        // winner can trigger the endgame and get the funds
-        // game must not be running
-        require(msg.sender == games[gameId].winner, "Only winner can trigger endgame");
-        require(games[gameId].isGameRunning == false, "Game is still running");
+    // end game and cashout
+    //TODO: emit winner list? for front end consumption
+    function endGame(uint256 gameId) private {
+        require (games[gameId].isGameRunning == false, "End game: cannot end running game");
         
-        // transfer prizePool to winner
-        address payable _winner = payable(games[gameId].winner);
-        _winner.transfer(games[gameId].prizePool);
+        uint256 numOfWinners = games[gameId].winnerList.length;
+        require (numOfWinners >= 0, "End game: invalid number of winners");
+
+        if (numOfWinners == 0) {
+            // TODO: Rollover some pot
+            // if number of winners == 0, 100% of the prize pool is kept as commission
+            contractOwnerCommission += games[gameId].prizePool;
+        } else {
+            // if number of winners > 0, prize pool divded evenly among winners
+            // insecure divide
+            uint256 prize = games[gameId].prizePool / numOfWinners; 
+            for (uint256 i = 0; i < numOfWinners; i++) {
+                address payable winner = payable(games[gameId].winnerList[i]);
+                winner.transfer(prize);
+            }
+
+        }
+
+        // endgame logistics
+        // mark globally all participatings players as currently not in any instance
+        for (uint8 i = 0; i < 4; i++) {
+            address player = games[gameId]._playerList[i];
+            playerList[player] = false;
+        }
+        delete games[gameId];
     }
 
     // main function to participate
-    // returns bool to indicate win or lost
-    function play(uint256 gameId, uint8 playerGuess) public payable returns(bool isGuessCorrect) {
-        // game must be running
-        // price to play must be payed
-        // game owner should not be able to participate [not implemented]
-        // the cup must not already be flipped
-        require(games[gameId].isGameRunning == true, "Game must be running to play");
-        require(msg.value >= games[gameId].entryCost, "Entry cost not met");        
-        require(ballBoardContract.isRevealed(games[gameId].boardId, playerGuess) == false, "Cup has been flipped already");
+    function play(uint8 playerGuess) public payable returns(uint256 gameId) {
+        require (msg.value == entryCost, "Play: entry cost not met");
+        require (msg.sender != contractOwner, "Play: Owner cannot play");
+        require (playerList[msg.sender] == false, "Play: player is already playing");
 
-        // owner take a cut
-        games[gameId].numPlayers++;
-        games[gameId].prizePool += msg.value - entryCommission;
-
-        isGuessCorrect = ballBoardContract.revealCup(games[gameId].boardId, playerGuess);
-        if (isGuessCorrect == true) {
-            // if win
-            // save winner address
-            // mark game as close
-            games[gameId].winner = msg.sender;
-            games[gameId].isGameRunning = false;
+        // find instance
+        if (runningGameQueue.isEmpty() == true) {
+            gameId = startGame();
+            runningGameQueue.enqueue(gameId);
         } else {
-            // if lose
-            // increase entryCost - POC increase arbitrarily e.g. *2
-            games[gameId].entryCost *= 2;
-        }  
+            gameId = runningGameQueue.peek();
+        }
+        require(games[gameId].isGameRunning == true, "Play: game is not running");
+
+        // game logistics
+        games[gameId].numPlayers++;
+        games[gameId].prizePool += msg.value;
+        games[gameId]._playerList.push(msg.sender);
+        playerList[msg.sender] = true;
+
+        // play game
+        bool isGuessCorrect = ballBoardContract.revealCup(games[gameId].boardId, playerGuess);
+        if (isGuessCorrect == true) {
+            games[gameId].winnerList.push(msg.sender);        
+        } 
+  
+        // TODO: potential race condition?
+        if (games[gameId].numPlayers == 4) {
+            games[gameId].isGameRunning = false;
+            uint256 endingGameId = runningGameQueue.dequeue();
+            require (endingGameId == gameId, "Play: temp throw");
+            endGame(gameId);
+        }
     }
 
     function getPrizePool(uint256 gameId) public view returns(uint256) {
@@ -99,13 +127,11 @@ contract GameMaster {
     }
 
     //TODO: Check logic
-    function withdraw(uint256 gameId) public gameOwnerOnly(gameId) {  
-        // game must not be running
-        require (games[gameId].isGameRunning == false, "Game must not be running to withdraw");
+    function withdraw() public {  
+        require(msg.sender == contractOwner, "Withdraw: only contract owner can withdraw");
         
-        // withdraw commissions
-        address payable _owner = payable(games[gameId].owner);
-        _owner.transfer(games[gameId].numPlayers * entryCommission);
+        address payable owner = payable(contractOwner);
+        owner.transfer(contractOwnerCommission);
     }
 
     function getGameRunning(uint256 gameId) public view returns(bool) {
@@ -116,16 +142,9 @@ contract GameMaster {
         return games[gameId].numPlayers;
     }
 
-    function getOwner(uint256 gameId) public view returns(address) {
-        return games[gameId].owner;
-    }
 
-    function getEntryCost(uint256 gameId) public view returns(uint256) {
-        return games[gameId].entryCost;
-    }
-
-    function getWinner(uint256 gameId) public view returns(address) {
-        return games[gameId].winner;
+    function getEntryCost() public view returns(uint256) {
+        return entryCost;
     }
 
     function getContractBalance() public view returns(uint256) {
